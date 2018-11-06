0c63f0e6db9514e27e00246d3f2433bde1a63e33
Calham Northway <cjnorthway@shaw.ca>
2018-11-06 14:32:22 -0800

    Built, gender Enum, UserVO, and added myself to user list in main java class

---
+++ app/src/main/java/com/example/calhamnorthway/group17projectpart4/UserVO.java
@@ -0,0 +1,67 @@
+package com.example.calhamnorthway.group17projectpart4;
+
+public class UserVO {
+    private String name;
+    private Integer age;
+    private Enum<Gender> gender;
+    private String description;
+    private UserVO[] matches;
+    private UserVO[] likedPeople;
+
+    public UserVO(String name, Integer age, Enum<Gender> gender, String description, UserVO[] matches, UserVO[] likedPeople){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this. description = description;
+        this.matches = matches;
+        this.likedPeople = likedPeople;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Enum<Gender> getGender() {
+        return gender;
+    }
+
+    public void setGender(Enum<Gender> gender) {
+        this.gender = gender;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public UserVO[] getMatches() {
+        return matches;
+    }
+
+    public void setMatches(UserVO[] matches) {
+        this.matches = matches;
+    }
+
+    public UserVO[] getLikedPeople() {
+        return likedPeople;
+    }
+
+    public void setLikedPeople(UserVO[] likedPeople) {
+        this.likedPeople = likedPeople;
+    }
+}
