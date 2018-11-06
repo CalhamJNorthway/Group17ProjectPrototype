b3832384496340596cf8c4b01d4a42c089f37997
Brianna <briannamarshinew@gmail.com>
2018-11-06 14:47:37 -0800

    added the rest of the user objects

--- app/src/main/java/com/example/calhamnorthway/group17projectpart4/MainActivity.java
+++ app/src/main/java/com/example/calhamnorthway/group17projectpart4/MainActivity.java
@@ -17,9 +17,18 @@ public class MainActivity extends AppCompatActivity {
 
         //Create user
         UserVO calham = new UserVO("Calham Northway", 21, Gender.Male, "Sexxxier than the Dos Equis guy.", null, null);
+        UserVO brianna = new UserVO("Brianna Marshinew", 21, Gender.Female, "I like long walks on the beach.", null, null);
+        UserVO michael = new UserVO("Michael CY", 22, Gender.Male, "We can hang.", null, null);
+        UserVO chris = new UserVO("Chris Beda", 26, Gender.Male, "Banned from Christian Mingle", null, null);
+
+
 
         //Add user to arrayList
         allUsers.add(calham);
+        allUsers.add(brianna);
+        allUsers.add(chris);
+        allUsers.add(michael);
+
 
 
     }
