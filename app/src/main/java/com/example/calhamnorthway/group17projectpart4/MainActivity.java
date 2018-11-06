c2eee2528b9f843556cc7c6224719940f6c3b0c6
Calham Northway <cjnorthway@shaw.ca>
2018-11-06 14:32:22 -0800

    Built, gender Enum, UserVO, and added myself to user list in main java class

--- app/src/main/java/com/example/calhamnorthway/group17projectpart4/MainActivity.java
+++ app/src/main/java/com/example/calhamnorthway/group17projectpart4/MainActivity.java
@@ -3,11 +3,24 @@ package com.example.calhamnorthway.group17projectpart4;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Instantiate User Array for app
+        ArrayList<UserVO> allUsers = new ArrayList<>();
+
+        //Create user
+        UserVO calham = new UserVO("Calham Northway", 21, Gender.Male, "Sexxxier than the Dos Equis guy.", null, null);
+
+        //Add user to arrayList
+        allUsers.add(calham);
+
+
     }
 }
