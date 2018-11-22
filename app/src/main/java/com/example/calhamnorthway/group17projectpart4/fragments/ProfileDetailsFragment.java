b48ac87dfd0af8faeae2ccff8bd02a7ca84c0fb3
Calham Northway <calham@rfgsoftware.com>
2018-11-21 22:46:24 -0800

    Layout For meet people page and details

--- app/src/main/java/com/example/calhamnorthway/group17projectpart4/fragments/ProfileDetailsFragment.java
+++ app/src/main/java/com/example/calhamnorthway/group17projectpart4/fragments/ProfileDetailsFragment.java
@@ -9,6 +9,7 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.calhamnorthway.group17projectpart4.MainActivity;
 import com.example.calhamnorthway.group17projectpart4.R;
@@ -43,6 +44,27 @@ public class ProfileDetailsFragment extends Fragment {
 
         ViewPager viewPager = v.findViewById(R.id.profilePicSlider);
         viewPager.setAdapter(new ViewPagerAdapter(getActivity(), potentialMatch.getProfile().getPictureIds()));
+
+        TextView name = v.findViewById(R.id.name);
+        name.setText(potentialMatch.getName());
+
+        TextView age = v.findViewById(R.id.age);
+        age.setText(potentialMatch.getAge()+"");
+
+        TextView relationShipStatus = v.findViewById(R.id.relationshipStatus);
+        relationShipStatus.setText(potentialMatch.getProfile().getRelationshipStatus().toString());
+
+        TextView matchedOn = v.findViewById(R.id.job);
+        matchedOn.setText(potentialMatch.getProfile().getJob());
+
+        TextView gender = v.findViewById(R.id.gender);
+        gender.setText(potentialMatch.getGender().toString());
+
+        TextView description = v.findViewById(R.id.description);
+        description.setText(potentialMatch.getProfile().getDescription());
+
+
+
 
         return v;
     }
